db774bb21c6299dc076c839eda26e66e5d92b219
Yashi Tiwari <yashidurgeshtiwari@gmail.com>
2025-07-14 00:19:28 +0530

    🔧 Fix: Bind Flask to 0.0.0.0 and dynamic port for Render

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
+from app.routes import main as app
+import os
 
-from flask import Flask
-from app.routes import main
-
-app = Flask(__name__)
-app.register_blueprint(main)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
